6e8fccb6508038d070520a6af6ff39978dacc306
Justin M. LaPre <justin@lapre.com>
2015-07-06 23:34:53 -0400

    Added LP::ID.

--- ROSS/LP.h
+++ ROSS/LP.h
@@ -1,11 +1,16 @@
 #ifndef LP_H
 #define LP_H
 
+#include <cstdint>
+
 class KP;
 
 class LP {
     /// The KP that "owns" this LP.
     KP *kp;
+
+public:
+    typedef uint64_t ID;
 };
 
 #endif /* LP_H */
