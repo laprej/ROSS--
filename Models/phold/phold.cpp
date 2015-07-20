efbd607e483651328d0b132408d7ebd34556e690
Justin M. LaPre <justin@lapre.com>
2015-07-20 18:04:12 -0400

    CRTP

--- Models/phold/phold.cpp
+++ Models/phold/phold.cpp
@@ -1,6 +1,31 @@
 #include "phold.h"
 
+#include <iostream>
+
+void Phold::mapping     (LP::ID globalID)
+{
+}
+void Phold::init        (Phold *st, LP *lp)
+{
+    std::cout << "Hello, world!\n";
+}
+void Phold::preRun      (Phold *st, LP *lp)
+{
+}
+void Phold::final       (Phold *st, LP *lp)
+{
+}
+void Phold::event       (Phold *st, LP *lp, Event *e)
+{
+}
+void Phold::reverseEvent(Phold *st, LP *lp, Event *e)
+{
+}
+
 int main(int argc, char *argv[])
 {
+    Phold foo;
+    foo.init(0, 0);
+
     return 0;
 }
--- Models/phold/phold.h
+++ Models/phold/phold.h
@@ -1,4 +1,17 @@
 #ifndef PHOLD_H
 #define PHOLD_H
 
+#include "LP.h"
+#include "Model.h"
+
+class Phold : Model<Phold> {
+public:
+    void mapping     (LP::ID globalID);
+    void init        (Phold *st, LP *lp);
+    void preRun      (Phold *st, LP *lp);
+    void final       (Phold *st, LP *lp);
+    void event       (Phold *st, LP *lp, Event *e);
+    void reverseEvent(Phold *st, LP *lp, Event *e);
+};
+
 #endif /* PHOLD_H */
