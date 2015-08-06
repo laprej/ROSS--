bd067d5e624f36f9fed160f1c0abd70e30df220b
Justin M. LaPre <justin@lapre.com>
2015-08-06 13:54:16 -0400

    Removed using namespace from header.

--- ROSS/Event.h
+++ ROSS/Event.h
@@ -7,14 +7,14 @@
 #include "globals.h"
 #include "Timestamp.h"
 
-using namespace boost::intrusive;
+namespace intrusive = boost::intrusive;
 
 namespace ROSS {
 
 class LP;
 
 /// We inherit from bs_set_base_hook to get hooks to support splay operations
-struct Event : public bs_set_base_hook<> {
+struct Event : public intrusive::bs_set_base_hook<> {
     Timestamp timeStamp;
     uint64_t eventID;
     std::bitset<64> bitField;
