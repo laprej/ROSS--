086eb3bde2819155a979897a7159b0bf1a2927f8
Justin M. LaPre <justin@lapre.com>
2015-08-06 00:36:53 -0400

    Added boost for splay trees.

--- ROSS/Event.h
+++ ROSS/Event.h
@@ -2,13 +2,19 @@
 #define ROSSMM_EVENT_H
 
 #include <bitset>
+#include <boost/intrusive/splaytree.hpp>
+#include <boost/intrusive/bs_set_hook.hpp>
 #include "globals.h"
 #include "Timestamp.h"
+
+using namespace boost::intrusive;
 
 namespace ROSS {
 
 class LP;
-struct Event {
+
+/// We inherit from bs_set_base_hook to get hooks to support splay operations
+struct Event : public bs_set_base_hook<> {
     Timestamp timeStamp;
     uint64_t eventID;
     std::bitset<64> bitField;
